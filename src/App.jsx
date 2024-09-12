ef8c7861a72a3fd6249d72b4c04080cccc832082
shailja <shailja7024@gmail.com>
2024-09-12 16:20:47 +0530

    Update navbar make it responsive and add toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ export default function App() {
     <>
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      {/* <Navbar handleThemeChange={handleThemeChange} themeMode={themeMode}/> */}
       <PortfolioPage handleThemeChange={handleThemeChange} themeMode={themeMode} />
     </ThemeProvider>        
       
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,125 @@
-import React from 'react';
+
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
 import reactLogo from '/dark_logo.png';
-import CustomizedSwitch from './Switch'
-import './Navbar.css'
+import CustomizedSwitch from './Switch';
+
+const options = ['Home', 'About Me', 'Skills', 'Projects', 'Contact Me'];
+const optionsId = ['#home', '#aboutme', '#skills', '#projects', '#contactme'];
 
 function Navbar({ handleThemeChange, themeMode }) {
-    const handleSwitchChange = (event) => {
-        handleThemeChange(event.target.checked ? 'dark' : 'light');
-      };
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const handleSwitchChange = (event) => {
+    handleThemeChange(event.target.checked ? 'dark' : 'light');
+  };
+
+  // Define styles based on themeMode
+  const appBarStyle = themeMode === 'dark' 
+    ? { bgcolor: '#121212', borderBottom: '1px solid #03ffff' }
+    : { bgcolor: '#fff', borderBottom: '1px solid #03ffff' };
+
+  const iconColor = themeMode === 'dark' ? '#fff' : '#212121';
 
   return (
-    
-    <div className={`nav ${themeMode}`}>
-      <ul>
-        <li className='nav-links'>
-            <a href="https://react.dev" target="_blank">
-                <img src={reactLogo} className="logo_react" alt="React logo"/>
+    <AppBar position="sticky" sx={{ ...appBarStyle, width: '100vw', boxShadow: 'none', padding: '0 8rem' }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>        
+          <Typography sx={{ 
+            position: { xs: 'absolute', md: 'static' },
+            top: 0,
+            left: 0,
+            display: { xs: 'none', md: 'block' },
+            width: '50px',
+            height: 'auto',
+             }}>
+            <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
+              <img src={reactLogo} className="logo_react" alt="React logo" 
+              style={{ height: '38px', borderRadius:'50%'}} />
             </a>
-        </li>
-        <li className={`nav-links active ${themeMode}`}><a href="#home">Home</a></li>
-        <li className='nav-links'><a href="#aboutme">About Me</a></li>
-        <li className='nav-links'><a href="#skills">Skills</a></li>
-        <li className='nav-links'><a href="#projects">Projects</a></li>
-        <li className='nav-links'><a href="#contactme">Contact</a></li>
-        <li className="theme-switch">
-        <CustomizedSwitch themeMode={themeMode} handleSwitchChange={handleSwitchChange} />
-        </li>
-        </ul>
-    </div>
-    
-  )
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color={iconColor}
+              sx={{ fontSize: 40 }} 
+            >
+              <MenuIcon sx={{ fontSize: 'inherit' }} />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: 'block', md: 'none' } }}
+            >
+              {options.map((option) => (
+                <MenuItem key={option} onClick={handleCloseNavMenu}>
+                  <Typography sx={{ textAlign: 'center', color: iconColor }}>{option}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          {options.map((option, index) => (
+              <Button
+                key={option}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: iconColor, display: 'block' }}
+              >
+                <a 
+                  href={optionsId[index]} 
+                  style={{ color: 'inherit', textDecoration: 'none', fontWeight:'600' }}
+                >
+                  {option}
+                </a>
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 0, color: iconColor }}>             
+            <CustomizedSwitch themeMode={themeMode} handleSwitchChange={handleSwitchChange} />        
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
 
 export default Navbar;
